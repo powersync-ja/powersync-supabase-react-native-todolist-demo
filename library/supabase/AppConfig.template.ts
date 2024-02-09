b3b20027cf0974fcb6c0bda8b5b03bddf6a99b53
benitav <benitav@users.noreply.github.com>
2024-02-09 16:40:09 +0200

    Clear out repo

--- library/supabase/AppConfig.template.ts
+++
@@ -1,9 +0,0 @@
-/**
- * TODO! Rename to AppConfig.ts and insert secrets
- */
-export const AppConfig = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  supabaseBucket: '', // Optional, set if you want to use attachments and upload to Supabase Storage
-  powersyncUrl: ''
-};
